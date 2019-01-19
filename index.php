d7db1943f1b9e4a8bbe9a957822ea4f5d41afedf
maximilian.goeckler <maximilian.goeckler@sz-ybbs.ac.at>
2019-01-20 00:02:10 +0100

    Neues Update

--- index.php
+++ index.php
@@ -13,13 +13,13 @@
 		<div class="row" style="height: 100%;">
 			<div class="col s6 black-half half">
 				<div class="row set-right">
-					<div class="box col s4 white">
+					<div class="box col s6 m3 white">
 						<div class="row">
 							<p id="day" class="col s12 big black-text"></p>
 							<p class="col s12 black-text">day</p>
 						</div>
 					</div>
-					<div class="box col s4 white">
+					<div class="box col s6 m3 white">
 						<div class="row">
 							<p id="hour" class="col s12 big black-text"></p>
 							<p class="col s12 black-text">hour</p>
@@ -29,13 +29,13 @@
 			</div>
 			<div class="col s6 white-half half">
 				<div class="row set-left">
-					<div class="box col s4 black">
+					<div class="box col s6 m3 black">
 						<div class="row">
 							<p id="min" class="col s12 big"></p>
 							<p class="col s12">min</p>
 						</div>
 					</div>	
-					<div class="box col s4 black">
+					<div class="box col s6 m3 black">
 						<div class="row">
 							<p id="sec" class="col s12 big"></p>
 							<p class="col s12">sec</p>
